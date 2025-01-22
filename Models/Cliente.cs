7833c3b4ef7754baf0f30dc1166ea0358280b252
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2025-01-22 17:22:30 -0300

    Modelo de cliente como en tabla

---
+++ Models/Cliente.cs
@@ -0,0 +1,10 @@
+namespace tl2_tp6_2024_Isas321.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
